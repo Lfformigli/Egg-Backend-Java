baf07baf61bb3e56dea21071e98ea8ca562ccca2
Facundo Formigli <lfformigli@gmail.com>
2023-03-13 22:26:19 -0300

    Encuentro5

--- Encuentro5/Ejercicio8.java
+++ Encuentro5/Ejercicio8.java
@@ -8,7 +8,7 @@ public class Ejercicio8 {
         Scanner read = new Scanner(System.in);
         int n = read.nextInt();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n ; i++) {
             for (int j = 0; j < n; j++) {
                 if (i==0 || i==n-1) {
                     System.out.print("*");
